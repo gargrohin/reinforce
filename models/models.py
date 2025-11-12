80da3ccbb6125edb3bd78639f729a687b78a73cf
rohinscl <rohin.garg@scale.com>
2025-11-12 22:22:15 +0000

    init code, models is wrong right now. from pretrained doesn't work.

---
+++ models/models.py
@@ -0,0 +1,295 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from dataclasses import dataclass
+from transformers import GPT2LMHeadModel
+
+# class LayerNorm(nn.Module):
+
+#     def __init__(self, features, bias=True):
+#         super().__init__()
+#         self.weight = nn.Parameter(torch.ones(features))
+#         self.bias = nn.Parameter(torch.zeros(features)) if bias else None
+    
+#     def forward(self, x):
+#         return F.layer_norm(x, self.weight.shape, self.weight, self.bias, eps=1e-5)
+# can just set bias = False to pytorch layer norm
+
+@dataclass
+class GPTConfig:
+    block_size: int  = 1024
+    vocab_size: int = 50304 # 50257 for GPT-2 uncased, 50280 for GPT-2 cased, padded up to nearest multiple of 64 for efficiency
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: bool = True
+
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+
+        # key query value projections for all heads, in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # output projection layer
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        # regularization
+        
+        self.n_embd = config.n_embd
+        self.n_head = config.n_head
+        self.dropout = nn.Dropout(config.dropout)
+
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+
+        if not self.flash:
+            # causal mask (is this already in flash attention?)
+            self.register_buffer("mask", 
+            torch.tril(torch.ones(config.block_size, config.block_size))
+            .view(1, 1, config.block_size, config.block_size))
+        
+
+
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, seq length, embed dim (n_embd)
+
+        # query key values for all heads in batch
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        # reshape q, k, v for head attention computation
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, head_dim)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, head_dim)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, head_dim)
+
+        # causal self attention: self attend: (B, nh, T, head_dim) x (B, nh, head_dim, T) -> (B, nh, T, T)
+
+        if self.flash:
+            # use flash attention cuda kernels
+            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        else:
+            # actual stuff
+            attn = (q @ k.transpose(-2,-1)) * (1.0 / math.sqrt(k.size(-1)))
+            attn = attn.masked_fill(self.mask[:, :, :T, :T] == 0, float('-inf'))
+            attn = F.softmax(attn, dim=-1)
+            attn = self.attn_dropout(attn)
+
+            y = attn @ v # (B, nh, T, T) x (B, nh, T, head_dim) -> (B, nh, T, head_dim)
+
+        y = y.transpose(1,2).contiguous().view(B, T, C)
+
+        #output projection
+        y = self.resid_dropout(self.c_proj(y))
+
+        return y
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        # 4 is a parameter transformer gods chose
+        self.c_fc = nn.Linear(config.n_embd, 4*config.n_embd, bias=config.bias)
+        self.gelu = nn.GELU()
+        self.c_proj = nn.Linear(4*config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+    
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd, bias=config.bias)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd, bias=config.bias)
+        self.mlp = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+    
+
+
+
+class Transformer(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            drop = nn.Dropout(config.dropout),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd, bias=config.bias),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # weight tying
+        self.transformer.wte.weight = self.lm_head.weight
+
+        #init weights
+        self.apply(self._init_weights)
+
+        # scaled init to residual projections:
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2*config.n_layer))
+        
+        print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
+    
+    def get_num_params(self, non_embeddings=True):
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embeddings:
+            n_params -= self.transformer.wpe.weight.numel()
+        return n_params
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        
+    
+    def forward(self, idx, targets=None):
+        b, t = idx.size()
+        device = idx.device
+        assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device)
+
+        #forward
+        tok_embd = self.transformer.wte(idx) # b, t, n_embd
+        pos_embd = self.transformer.wpe(pos) # t, n_embd
+
+        x = self.transformer.drop(tok_embd + pos_embd)
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x)
+
+        if targets is not None: 
+            # calc loss
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(logits.view(-1 , logits.size(-1)), targets.view(-1), ignore_index=-1)
+        else:
+            # inference time
+            logits = self.lm_head(x)[:, [-1], :] # list [-1] preserves time dimensionnnnn
+            loss = None
+        
+        return logits, loss
+    
+    def crop_block_size(self, block_size):
+        # model surgery bwahaha
+        self.config.block_size = block_size
+        self.transformer.wpe.weight = nn.Parameter(self.transformer.wpe.weight[:block_size])
+        for block in self.transformer.h:
+            if hasattr(block.attn, 'mask'):
+                block.attn.mask = block.attn.mask[:, :, :block_size, :block_size]
+    
+    @classmethod
+    def from_pretrained(cls, model_type, override_args=None):
+
+        override_args = override_args or {}
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        
+        assert all(k == 'dropout' for k in override_args)
+
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+
+        print("forcing vocab_size=50257, block_size=1024, bias=True")
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        config_args['bias'] = True # always True for GPT model checkpoints
+
+        if 'dropout' in override_args:
+            print(f"overriding dropout rate to {override_args['dropout']}")
+            config_args['dropout'] = override_args['dropout']
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = Transformer(config)
+
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.mask')] # discard the mask / buffer, not a param
+
+        # huggingface
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy params
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        
+        return model
+
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens, temperature = 1.0, top_k=None):
+        
+        for _ in range(max_new_tokens):
+            #crop if too long
+            idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+            #forward
+            logits, _ = self(idx_cond)
+            # final step
+            logits = logits[:, -1, :] / temperature
+
+            # crop for top k
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float("Inf") # mask
+            
+            # softmax
+            probs = F.softmax(logits, dim=-1)
+
+            # sample and append
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        
+        return idx
+             
+
+    
+
+
+
+
+
+
+        
+
